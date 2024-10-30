8950686fdedefa62e6a796f0f7dbb6443e531326
Sean <salmonseanz@gmail.com>
2024-10-30 13:43:16 +0800

    Minor Changes

--- Lab2/4_Counter/RTL/Counter.v
+++ Lab2/4_Counter/RTL/Counter.v
@@ -10,17 +10,17 @@ reg [24:0] counter;
 always@(posedge sys_clk or negedge sys_rst_n) begin //One should use or to connect 
 	if(sys_rst_n == 1'b0) //reset counter and light
 	begin
-		counter <= 1'b0;
+		counter <= 1'h0;
 		led_out <= 1'b1;
 	end
-	else if(counter == 1'b0) //counter reset if 1s is end and reverse light
+	else if(counter <= 1'h0) //counter reset if 1s is end and reverse light
 	begin
 		counter = 25'h2499_9999;
-		led_out <= !led_out;
+		led_out <= !led_out; //!led_out
 	end
 	else
 	begin
-		counter <= counter - 1'b1;
+		counter <= counter - 1'h1; //counting down
 	end
 end
 endmodule
