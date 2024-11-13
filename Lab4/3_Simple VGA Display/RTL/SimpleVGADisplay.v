8a8a8907893184f0b2db4ceaa0e0053e198a9aa2
Sean <salmonseanz@gmail.com>
2024-11-13 20:05:48 +0800

    Code Updated

--- Lab4/3_Simple VGA Display/RTL/SimpleVGADisplay.v
+++ Lab4/3_Simple VGA Display/RTL/SimpleVGADisplay.v
@@ -1,9 +1,6 @@
 module SimpleVGADisplay(
 input wire vga_clk, //clock of VGA, frequency is 25M Hz
 input wire sys_rst_n,
-input wire [9:0] pix_x, //pixel information in x-axis, 10bits
-input wire [9:0] pix_y, //pixel information in y-axis, 10bits
-output reg [15:0] pix_data //colour of the pixel output, 16 bits
 output wire hsync , //Line sync signal
 output wire vsync , //Field sync signal
 output wire [15:0] rgb //RGB565 data
